7abe6d8b6eddc2132e18f2be3926b602ffabcd08
abdonaser <abdonaser4223@gmail.com>
2024-07-24 06:31:12 +0300

    handelStructure

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import log from "../Styles/Login.module.css";
+const Login = () => {
+  return (
+    <div>
+      <h1 className={log.header}> login header</h1>
+    </div>
+  );
+};
+
+export default Login;
